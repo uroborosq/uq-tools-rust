a7709c02e35a54b4d66d61760208e143d834ce5e
Dmitriy Tikhomirov <dmitriy.tikhomirov@yadro.com>
2025-07-12 13:20:10 +0300

    s

--- src/temperature/sensors.rs
+++ src/temperature/sensors.rs
@@ -5,30 +5,24 @@ use std::{
     path::Path,
 };
 
-#[derive(Debug)]
-pub struct Sensor {
-    sensor_id: String,
-    value: f64,
-}
-
 pub fn get_sensors(sysfs_sensors_path: &Path) -> Result<collections::HashMap<String, collections::HashMap<String, f64>>, Box<dyn Error>> {
     let mut monitors_map: HashMap<String, HashMap<String, f64>> = HashMap::new();
 
     let monitor_dirs =
-        fs::read_dir(sysfs_sensors_path).map_err(|e| io::Error::new(e.kind(), format!("can't open sensors directory due to error: {:?}", e)))?;
+        fs::read_dir(sysfs_sensors_path).map_err(|e| io::Error::new(e.kind(), format!("can't open sensors directory due to error: {e}")))?;
 
     for monitor in monitor_dirs {
         let mut monitor_name: Option<String> = None;
 
-        let monitor_dir = monitor.map_err(|e| io::Error::new(e.kind(), format!("can't access monitor directory due to error: {:?}", e)))?;
+        let monitor_dir = monitor.map_err(|e| io::Error::new(e.kind(), format!("can't access monitor directory due to error: {e}")))?;
 
         let path = monitor_dir.path();
 
-        let sensor_files = fs::read_dir(path.clone())
-            .map_err(|e| io::Error::new(e.kind(), format!("can't open {:?} monitor directory due to error: {:?}", path, e)))?;
+        let sensor_files =
+            fs::read_dir(path.clone()).map_err(|e| io::Error::new(e.kind(), format!("can't open {path:?} monitor directory due to error: {e}")))?;
 
         for sensor_file_res in sensor_files {
-            let sensor_file = sensor_file_res.map_err(|e| io::Error::new(e.kind(), format!("can't open sensor file due to error: {:?}", e)))?;
+            let sensor_file = sensor_file_res.map_err(|e| io::Error::new(e.kind(), format!("can't open sensor file due to error: {e}")))?;
 
             // TODO: i wanna make it one line
             let file_name_os = sensor_file.file_name();
@@ -36,18 +30,18 @@ pub fn get_sensors(sysfs_sensors_path: &Path) -> Result<collections::HashMap<Str
                 .to_str()
                 .ok_or_else(|| format!("file name is not in Unicode: {:?}", sensor_file.file_name()))?;
 
-            println!("{}", file_name);
+            println!("{file_name}");
 
             // name of monitor
             if file_name == "name" {
                 monitor_name = Some(file_name.to_owned());
             }
 
-            let Some((prefix, suffix)) = file_name.split_once("_") else {
+            let Some((sensor_id, sensor_type)) = file_name.split_once("_") else {
                 continue;
             };
 
-            match suffix {
+            match sensor_type {
                 "label" => {
                     let sensor_label = fs::read_to_string(sensor_file.path())?;
                 }
